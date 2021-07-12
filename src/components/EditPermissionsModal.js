28d6fd52f166eb89db3d9cdb5a0eb0b11325dfa4
Arty Fact <chris.bush+arty-fact@mongodb.com>
2021-07-12 20:10:23 +0000

    Update Initiated by https://github.com/mongodb/docs-realm/commit/d761190267332ecd6908364f1b521eeefc884bc6

--- src/components/EditPermissionsModal.js
+++ src/components/EditPermissionsModal.js
@@ -15,7 +15,7 @@ function useTeamMembers() {
   const [teamMembers, setTeamMembers] = React.useState(null);
   const [newUserEmailError, setNewUserEmailError] = React.useState(null);
   const app = useRealmApp();
-  const { addTeamMember, removeTeamMember, getMyTeamMembers } = app.currentUser.functions;
+  const { addTeamMember, removeTeamMember, getMyTeamMembers } = app.functions;
   const updateTeamMembers = () => {
     getMyTeamMembers().then(setTeamMembers);
   };
